33747dc154186967d462baf540c130b7854baec9
snake <snake@mys4s.cn>
2022-11-07 11:14:29 +0800

    timstamp parse unix

--- timestamp/main.go
+++ timestamp/main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(time.Now().Unix())
+	if len(os.Args) > 1 {
+		i, err := strconv.ParseInt(os.Args[1], 10, 64)
+		if nil != err {
+			panic(err)
+		}
+		fmt.Println(time.Unix(i, 0))
+	} else {
+		fmt.Println(time.Now().Unix())
+	}
 }
