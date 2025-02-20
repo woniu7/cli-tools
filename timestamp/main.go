2a789caed6d1f2030f7965024b18f58205201428
wrd <wangerda@outlook.com>
2025-02-20 04:59:11 +0000

    短叹长吁，千声万声；捣枕捶床，到三更四更。便似止渴思梅，充饥画饼，因甚顷刻体？则伤我取次成。好个个舒心，干支刺没兴。

--- timestamp/main.go
+++ timestamp/main.go
@@ -11,9 +11,13 @@ func main() {
 	if len(os.Args) > 1 {
 		i, err := strconv.ParseInt(os.Args[1], 10, 64)
 		if nil != err {
-			panic(err)
+			//panic(err)
+			t, err := time.ParseInLocation("2006-01-02 15:04:05", os.Args[1], time.Local)
+			if nil != err { panic(err) }
+			fmt.Println(t.Unix())
+		} else {
+			fmt.Println(time.Unix(i, 0))
 		}
-		fmt.Println(time.Unix(i, 0))
 	} else {
 		fmt.Println(time.Now().Unix())
 	}
